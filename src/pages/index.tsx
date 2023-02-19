430162bc12ca8f65ab8c34f94cccca4a9e672cf2
Kevin Lowe <lowekevin123@gmail.com>
2023-02-18 20:33:38 -0800

    Destructure var

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,20 @@ import { api } from "../utils/api";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  const cards = api.cards.twoRandomCards.useQuery(undefined);
+  const { data: cards } = api.cards.twoRandomCards.useQuery(undefined);
 
   const handleClick1 = React.useCallback(() => {
-    if (cards.data?.id1 == null) {
+    if (cards?.id1 == null) {
       return;
     }
-    console.log("I got clicked", cards.data.id1);
-  }, [cards.data?.id1]);
+    console.log("I got clicked", cards.id1);
+  }, [cards?.id1]);
   const handleClick2 = React.useCallback(() => {
-    if (cards.data?.id2 == null) {
+    if (cards?.id2 == null) {
       return;
     }
-    console.log("I got clicked", cards.data.id2);
-  }, [cards.data?.id2]);
+    console.log("I got clicked", cards.id2);
+  }, [cards?.id2]);
 
   return (
     <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16 ">
@@ -28,13 +28,13 @@ const Home: NextPage = () => {
       </div>
       <div className="grid grid-cols-2 justify-items-center gap-4 md:gap-8">
         <Card
-          image_url={cards.data?.image_url1}
-          name={cards.data?.name1}
+          image_url={cards?.image_url1}
+          name={cards?.name1}
           handleClick={handleClick1}
         />
         <Card
-          image_url={cards.data?.image_url2}
-          name={cards.data?.name2}
+          image_url={cards?.image_url2}
+          name={cards?.name2}
           handleClick={handleClick2}
         />
       </div>
