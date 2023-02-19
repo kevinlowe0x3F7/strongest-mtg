3dd48b139e03b243e065d43daa7cb7e3bd88f49c
Kevin Lowe <lowekevin123@gmail.com>
2023-02-18 18:21:55 -0800

    Add cards API types

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "./trpc";
 import { cardsRouter } from "./routers/cards";
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
 /**
  * This is the primary router for your server.
@@ -12,3 +13,9 @@ export const appRouter = createTRPCRouter({
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
+
+type RouterInput = inferRouterInputs<AppRouter>;
+type RouterOutput = inferRouterOutputs<AppRouter>;
+
+export type CardsRandomCardInput = RouterInput["cards"]["randomCard"];
+export type CardsRandomCardOutput = RouterOutput["cards"]["randomCard"];
