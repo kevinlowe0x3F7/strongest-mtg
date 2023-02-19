e0bff0db0960089119b2c1791d15f22339b5918d
Kevin Lowe <lowekevin123@gmail.com>
2023-02-18 18:56:31 -0800

    Format app a bit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,64 @@
 import * as React from "react";
-import { Button } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { type NextPage } from "next";
-import Link from "next/link";
 
 import { api } from "../utils/api";
 import Image from "next/image";
+import { CardsRandomCardOutput } from "../server/api/root";
 
 const Home: NextPage = () => {
-  const card = api.cards.randomCard.useQuery(undefined);
-  console.log("card", card);
+  const card1 = api.cards.randomCard.useQuery(undefined);
+  const card2 = api.cards.randomCard.useQuery(undefined);
 
-  const handleClick = React.useCallback(() => console.log("I got clicked"), []);
+  const handleClick1 = React.useCallback(
+    () => console.log("I got clicked", card1),
+    [card1]
+  );
+  const handleClick2 = React.useCallback(
+    () => console.log("I got clicked", card2),
+    [card2]
+  );
   return (
     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
       <div className="text-3xl font-semibold text-white">
         Which card is stronger?
       </div>
-      <div className="grid grid-cols-2 grid-rows-1 justify-items-center gap-4 md:gap-8">
-        <Link
-          className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-          href="https://create.t3.gg/en/usage/first-steps"
-          target="_blank"
-        >
-          <h3 className="text-2xl font-bold">First Steps →</h3>
-          <div className="text-lg">
-            Just the basics - Everything you need to know to set up your
-            database and authentication.
-          </div>
-        </Link>
-        <Link
-          className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-          href="https://create.t3.gg/en/introduction"
-          target="_blank"
-        >
-          <h3 className="text-2xl font-bold">Documentation →</h3>
-          <div className="text-lg">
-            Learn more about Create T3 App, the libraries it uses, and how to
-            deploy it.
-          </div>
-        </Link>
-        <Button onClick={handleClick} variant="contained">
-          Stronger
-        </Button>
-        <Button onClick={handleClick} variant="contained">
-          Stronger
-        </Button>
-      </div>
-      <p className="text-2xl text-white">
-        {card.data ? card.data.name : "Loading tRPC query..."}
-        {card.data?.image_url ? (
-          <Image
-            src={card.data.image_url}
-            alt={card.data.name}
-            width={244}
-            height={340}
-          />
+      <div className="grid grid-cols-2 justify-items-center gap-4 md:gap-8">
+        {card1.data == null ? (
+          <CircularProgress />
         ) : (
-          "Loading image"
+          <LoadedCard card={card1.data} handleClick={handleClick1} />
         )}
-      </p>
+        {card2.data == null ? (
+          <CircularProgress />
+        ) : (
+          <LoadedCard card={card2.data} handleClick={handleClick2} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+interface LoadedCardProps {
+  card: CardsRandomCardOutput;
+  handleClick: () => void;
+}
+
+const LoadedCard: React.FC<LoadedCardProps> = ({ card, handleClick }) => {
+  const image =
+    card.image_url != null ? (
+      <Image src={card.image_url} alt={card.name} width={244} height={340} />
+    ) : (
+      <div>Card image not found</div>
+    );
+
+  return (
+    <div className="flex flex-col items-center">
+      <div>{card.name}</div>
+      {image}
+      <Button onClick={handleClick} variant="contained">
+        Stronger
+      </Button>
     </div>
   );
 };
--- src/server/api/routers/cards.ts
+++ src/server/api/routers/cards.ts
@@ -3,11 +3,19 @@ import * as Scry from "scryfall-sdk";
 
 export const cardsRouter = createTRPCRouter({
   randomCard: publicProcedure
-    .query(async () => {
-      const randomCard = await Scry.Cards.random(); 
+    .query(() => {
+      /*
+      const [randomCard, anotherRandomCard] = await Promise.all([Scry.Cards.random(), Scry.Cards.random()]);
       return {
+        id: randomCard.oracle_id,
         name: randomCard.name,
         image_url: randomCard.getImageURI("normal"),
+      };
+      */
+      return {
+        id: "89c5dec1-cefe-4ce3-a9df-b5cb9fa73b35",
+        name: "Twisted Justice",
+        image_url: "https://cards.scryfall.io/normal/front/d/8/d8efa02d-c301-47e1-8cdf-26ff9e97a243.jpg?1598917592",
       };
     }),
 });
