6151832bd93a91283dc395e51c6dcd6e5b438551
Kevin Lowe <lowekevin123@gmail.com>
2023-02-21 22:54:54 -0800

    Implement navbar

---
+++ src/components/navBar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import * as React from "react";
+
+export const NavBar: React.FC = () => {
+  return (
+    <div className="col-span-1 flex h-10 w-full items-center gap-3 border-b-[1px] border-gray-50 bg-[#2e026d] p-2">
+      <Link className="text-white" href="/">
+        Home
+      </Link>
+      <Link className="text-white" href="/">
+        Results
+      </Link>
+    </div>
+  );
+};
